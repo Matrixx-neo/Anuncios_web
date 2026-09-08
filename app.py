22dabf48fc1363d41887b28b842c0a907cb76e86
Matrixx-neo <best.bokutop@gmail.com>
2026-09-07 19:00:15 -0500

    fix: updated TemplateResponse syntax for Starlette

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
